794606ad23ab42d8320420039dfb722e8f515b05
TheRealSvc <svcoelln@gmail.com>
2021-07-25 10:58:45 +0200

    tiny changes

--- client/src/components/CourseElement.js
+++ client/src/components/CourseElement.js
@@ -4,15 +4,19 @@ import React from 'react';
 const courseElement = (props) => {
     console.log(`in Course: ${props.course.title}`);
 
-
     return (  
-     <tbody>
-       <tr>
-         <td> <a href={`/courseDetails/${props.course[0]}`}> {props.course[0]} </a></td>
-         <td> {props.course[1]} </td>
-         <td> {props.course[2]} </td>
-       </tr>
-     </tbody>
+    <a className="course--module course--link" href="course-detail.html">
+      <h2 className="course--label">Course</h2>
+      <h3 className="course--title">{props.course[1]}</h3>
+    </a>
+
+     //<tbody>
+     //  <tr>
+     //    <td> <a href={`/courseDetails/${props.course[0]}`}> {props.course[0]} </a></td>
+      //   <td> {props.course[1]} </td>
+      //   <td> {props.course[2]} </td>
+     //  </tr>
+    // </tbody>
     );
 }
 
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,7 +1,63 @@
 import React, { Component } from 'react'; 
 import CourseElement from './CourseElement';
-import '../index.css'; 
+import '../global.css';
 
+/*
+class Courses extends Component {
+  render: function() {
+    return (
+      <div>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="Treehouse Full Stack JavaScript Project 10 | Full Stack App with React and a REST API" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon" />
+        <title>Courses</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap" rel="stylesheet" />
+        <link href="../styles/reset.css" rel="stylesheet" />
+        <link href="../styles/global.css" rel="stylesheet" />
+        <div id="root">
+          <header>
+            <div className="wrap header--flex">
+              <h1 className="header--logo"><a href="index.html">Courses</a></h1>
+              <nav>
+                <ul className="header--signedout">
+                  <li><a href="sign-up.html">Sign Up</a></li>
+                  <li><a href="sign-in.html">Sign In</a></li>
+                </ul>
+              </nav>
+            </div>
+          </header>
+          <main>
+            <div className="wrap main--grid">
+              <a className="course--module course--link" href="course-detail.html">
+                <h2 className="course--label">Course</h2>
+                <h3 className="course--title">Build a Basic Bookcase</h3>
+              </a>
+              <a className="course--module course--link" href="course-detail.html">
+                <h2 className="course--label">Course</h2>
+                <h3 className="course--title">Learn How to Program</h3>
+              </a>
+              <a className="course--module course--link" href="course-detail.html">
+                <h2 className="course--label">Course</h2>
+                <h3 className="course--title">Learn How to Test Programs</h3>
+              </a>
+              <a className="course--module course--add--module" href="create-course.html">
+                <span className="course--add--title">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " /></svg>
+                  New Course
+                </span>
+              </a>
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+});
+}
+*/
 
 /*
 Courses - This component provides the "Courses" screen by retrieving the list of courses from the REST API's /api/courses route and rendering a list of courses. 
@@ -25,22 +81,44 @@ class Courses extends Component {
         };
 
         render() {
-            return (
-                <div className="courses-container">
-                 <h2> Courses </h2>    
-                 <table>
-                    <thead>
-                     <tr>
-                     <th> id </th>
-                     <th> Title </th>
-                     <th> Duration </th>
-                     </tr>
-                    </thead>
-                        {this.createCourseArray(this.props.courses)}
-                </table>
-                </div > 
-                )
-            }
-        }
-
+          return (
+            <div>
+              <meta charSet="UTF-8" />
+              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+              <meta name="description" content="Treehouse Full Stack JavaScript Project 10 | Full Stack App with React and a REST API" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon" />
+              <title>Courses</title>
+              <link rel="preconnect" href="https://fonts.gstatic.com" />
+              <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap" rel="stylesheet" />
+              <link href="../styles/reset.css" rel="stylesheet" />
+              <link href="../styles/global.css" rel="stylesheet" />
+              <div id="root">
+                <header>
+                  <div className="wrap header--flex">
+                    <h1 className="header--logo"><a href="index.html">Courses</a></h1>
+                    <nav>
+                      <ul className="header--signedout">
+                        <li><a href="sign-up.html">Sign Up</a></li>
+                        <li><a href="sign-in.html">Sign In</a></li>
+                      </ul>
+                    </nav>
+                  </div>
+                </header>
+                <main>
+                  <div className="wrap main--grid">
+                  {this.createCourseArray(this.props.courses)}
+                    <a className="course--module course--add--module" href="create-course.html">
+                      <span className="course--add--title">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " /></svg>
+                        New Course
+                      </span>
+                    </a>
+                  </div>
+                </main>
+              </div>
+            </div>
+          );
+        };
+      }
 export default Courses;
